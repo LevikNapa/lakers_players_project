71ee7ebc37534281bdb02575aa525a42686634b8
github username <github email address>
2019-04-08 17:19:22 -0400

    refactor by adding new methods

--- lib/lakers_players/cli.rb
+++ lib/lakers_players/cli.rb
@@ -5,21 +5,27 @@ class CLI
   end
 
   def menu
-    puts "Please hit 'enter' to see a list of the lakers players"
+    puts "\nPlease type 'enter' to see a list of the lakers players"
     input = gets.strip
     if input == "enter"
     Scraper.new.page_scraper
-    menu
-    puts "Type a players number to see more info on him"
-    input = gets.strip.downcase
+    list_players
+    select_player
     end
   end
 
-  end
   def list_players
     Player.all.each.with_index(1) do |player, idx|
       puts "#{idx}. #{player.player_name}"
     end
+  end
+
+  def select_player
+    puts "\nType a players number to see more info on him:"
+    input = gets.strip.to_i
+    if input.between?(1,17)
+      number = Player.all[input-1]
+      display_player_info(number)
   end
 
 end
