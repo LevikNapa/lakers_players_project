c2fc73371c725a87174bca0d7eae2eb10cd3c80e
github username <github email address>
2019-04-08 16:49:51 -0400

    refactor

--- lib/lakers_players/cli.rb
+++ lib/lakers_players/cli.rb
@@ -1,16 +1,25 @@
 class CLI
   def start
-
     puts "Welcome to Lakers players!"
-    Scraper.new.page_scraper
     menu
   end
 
   def menu
-   Player.all.each.with_index(1) do |player, idx|
-     puts "#{idx}. #{player.player_name}"
-   end
+    puts "Please hit 'enter' to see a list of the lakers players"
+    input = gets.strip
+    if input == "enter"
+    Scraper.new.page_scraper
+    menu
+    puts "Type a players number to see more info on him"
+    input = gets.strip.downcase
+    end
+  end
 
+  end
+  def list_players
+    Player.all.each.with_index(1) do |player, idx|
+      puts "#{idx}. #{player.player_name}"
+    end
   end
 
 end
